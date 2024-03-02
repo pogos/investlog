da16a70e3f217817c088fa4ede305a5d3512afd5
pogos <pogos@o2.pl>
2024-03-02 23:28:26 +0100

    Add asset service

---
+++ services/asset-service/src/test/kotlin/pl/pogos/investlog/account/AssetServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.pogos.investlog.account
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AssetServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
