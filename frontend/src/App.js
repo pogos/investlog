9222a2719e30f4c92a30dd63ad91e1b1b456ee71
pogos <pogos@o2.pl>
2024-03-03 23:11:43 +0100

    Add bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { Container, Row, Col, Card } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Container fluid>
+            <Row>
+                <Col md={4}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Portfolio Overview</Card.Title>
+                            {/* Add content for portfolio overview */}
+                        </Card.Body>
+                    </Card>
+                </Col>
+                <Col md={8}>
+                    <Card>
+                        <Card.Body>
+                            <Card.Title>Investment Logs</Card.Title>
+                            {/* Add content for investment logs */}
+                        </Card.Body>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
+    );
 }
 
 export default App;
