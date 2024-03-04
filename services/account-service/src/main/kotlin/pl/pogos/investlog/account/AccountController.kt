160860e572762ba35245639cce2ccb6c5f38a0fd
pogos <pogos@o2.pl>
2024-03-04 22:09:18 +0100

    Add account simple model

---
+++ services/account-service/src/main/kotlin/pl/pogos/investlog/account/AccountController.kt
@@ -0,0 +1,15 @@
+package pl.pogos.investlog.account
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import pl.pogos.investlog.account.model.Account
+import java.util.UUID
+
+@RestController
+class AccountController {
+
+    @GetMapping
+    fun getAccounts(): List<Account> {
+        return listOf(Account(UUID.randomUUID().toString(), "Test account", "My first test account"))
+    }
+}
---
+++ services/account-service/src/main/kotlin/pl/pogos/investlog/account/model/Account.kt
@@ -0,0 +1,7 @@
+package pl.pogos.investlog.account.model
+
+data class Account(
+    val uuid: String?,
+    val name: String,
+    val description: String?,
+)
