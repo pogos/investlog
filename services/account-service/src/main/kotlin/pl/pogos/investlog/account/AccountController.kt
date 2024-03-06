07b08b6d9d008084902628f0dbe02ade1bd0ac2e
pogos <pogos@o2.pl>
2024-03-06 23:04:05 +0100

    Add save method

--- services/account-service/src/main/kotlin/pl/pogos/investlog/account/AccountController.kt
+++ services/account-service/src/main/kotlin/pl/pogos/investlog/account/AccountController.kt
@@ -1,6 +1,8 @@
 package pl.pogos.investlog.account
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import pl.pogos.investlog.account.model.Account
 import java.util.UUID
@@ -11,5 +13,10 @@ class AccountController {
     @GetMapping
     fun getAccounts(): List<Account> {
         return listOf(Account(UUID.randomUUID().toString(), "Test account", "My first test account"))
+    }
+
+    @PostMapping
+    fun save(@RequestBody account: Account) {
+
     }
 }
